f6622a4ff3feefda7e38906d4b748df155389627
walid khouaji <wkhouaji@u-pro.fr>
2019-04-30 17:31:36 +0200

    Updates

---
+++ precache-manifest.c78a2b5f35b418c1dba186be3c5928a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b86b80d71c4222b0f1d855fd91ba8ce",
+    "url": "/spikes/index.html"
+  },
+  {
+    "revision": "5181761e3777321ba61a",
+    "url": "/spikes/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/spikes/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5181761e3777321ba61a",
+    "url": "/spikes/static/js/main.c7b4a817.chunk.js"
+  },
+  {
+    "revision": "ac7a02a2ad22bfa2e1b5",
+    "url": "/spikes/static/js/runtime~main.d4fd1c86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spikes/static/media/logo.5d5d9eef.svg"
+  }
+]);
